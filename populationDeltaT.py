8a4db9daa20baddc10a956ff402dc6f267345cbc
djanluka <cvitak.boris@gmail.com>
2021-05-18 17:30:21 +0200

    NEW MODEL

---
+++ populationDeltaT.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+from random import *
+from numpy import *
+import sys
+
+#parametri
+a = 0.5 # prirodni prirastaj
+b = 0.5 # stopa umanjenja populacije vrste na osnovu uticaja toksina
+c = 0.5 # stopa izumiranja toksina
+d = 0.5 # stopa nastajanja toksina zavisi samo od kolicine populacije vrste
+dt = 0.01 
+max_time = 100
+
+# pocetne tacke
+t = 0
+x = 2 
+y = 1
+
+#incijalizacija listi za cuvanje podataka
+t_list = []; x_list = []; y_list = []
+
+t_list.append(t); x_list.append(x); y_list.append(y)
+
+while t < max_time:
+    x_t = x                     # prilikom promene Y, mora se uzeti X(t), a ne X(t+dt)
+    
+    t = t + dt
+    x = x + (a*x - b*x*y)*dt    #dX/dt = a*X - b*X*Y
+    y = y + (d*x_t - c*y)*dt    #dY/dt = -c*Y + d*X
+
+    t_list.append(t)
+    x_list.append(x)
+    y_list.append(y)
+
+p = plt.plot(t_list, x_list, 'r', t_list, y_list, 'b', linewidth = 1.5)
+plt.grid()
+plt.legend(['Alge', 'Toksini'], loc='upper right')
+plt.xlabel('vreme')
+plt.ylabel('populacija')
+plt.title(f'a: {a}, b: {b}, c: {c}, d: {d}')
+plt.show()
--- populationOdeint.py
+++ populationOdeint.py
@@ -2,27 +2,27 @@ from numpy import *
 import pylab as p
 
 # parametri 
-a = 0.8 # natalitet algi
-b = 0.4
-c = 0.7 # mortalitet toksina
-d = 0.2
+a = 0.5 # prirodni prirastaj
+b = 0.5 # stopa umanjenja populacije vrste na osnovu uticaja toksina
+c = 0.5 # stopa izumiranja toksina
+d = 0.5 # stopa nastajanja toksina zavisi samo od kolicine populacije vrste
 
-#dx/dt = a*x - b*x*y
-#dy/dt = -c*y + d*x*y
+#dX/dt = a*X - b*X*Y
+#dY/dt = -c*Y + d*X
 def dX_dt(X, t=0):
     return array([ a*X[0] - b*X[0]*X[1] ,  
-                  -c*X[1] + d*X[0]*X[1] ])
+                  -c*X[1] + d*X[0]      ])
 
 # stacionarne tacke
-X_f0 = array([ 0., 0.])
-X_f1 = array([ c/d, a/b])
+print(f'Stacionarne tacke : {(c*a) / (d*b)}, {a/b}')
+X_f1 = array([ (c*a) / (d*b), a/b])
 
 
 from scipy import integrate
 
 # vreme
-t = linspace(0, 50,  1000)
-# pocetne tacke:
+t = linspace(0, 100,  1000)
+# pocetne tacke
 X0 = array([2, 1])
 
 X = integrate.odeint(dX_dt, X0, t)
@@ -30,18 +30,18 @@ X = integrate.odeint(dX_dt, X0, t)
 algae, toxins = X.T
 
 f1 = p.figure()
-p.plot(t, algae, 'r-', label='Algae')
-p.plot(t, toxins  , 'b-', label='Toxins')
+p.plot(t, algae, 'r-', label='Alge')
+p.plot(t, toxins  , 'b-', label='Toksini')
 p.grid()
 p.legend(loc='upper right')
-p.xlabel('time')
-p.ylabel('population')
+p.xlabel('vreme')
+p.ylabel('populacija')
 p.title(f'a: {a}, b: {b}, c: {c}, d: {d}')
 #f1.savefig('./plots/model1.png')
-#p.show()
+p.show()
 
 #############################################################################################
-
+'''
 #vrednosti izmedju dve stacionarne tacke (0, c/d)
 values  = linspace(0, 3, 8)
 vcolors = p.cm.autumn_r(linspace(0., 1., len(values)))
@@ -72,12 +72,12 @@ DY1 /= M
 #iscrtavanje grafika
 p.title(f'Trajectories: a: {a}, b: {b}, c: {c}, d: {d}')
 Q = p.quiver(X1, Y1, DX1, DY1, M, pivot='mid', cmap=p.cm.jet)
-p.xlabel('Population of algae')
-p.ylabel('Population of toxins')
+p.xlabel('Alge')
+p.ylabel('Toksini')
 p.legend()
 p.grid()
 p.xlim(0, xmax)
 p.ylim(0, ymax)
 p.show()
 #f2.savefig('plots/populations.png')
-
+'''
