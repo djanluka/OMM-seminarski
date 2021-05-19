1b40d2b68847dfb0a4693a344aa946ce2fdaaaae
djanluka <cvitak.boris@gmail.com>
2021-05-19 17:53:41 +0200

    New parameters

--- populationOdeint.py
+++ populationOdeint.py
@@ -2,15 +2,15 @@ from numpy import *
 import pylab as p
 
 # parametri 
-a = 0.5 # prirodni prirastaj
-b = 0.5 # stopa umanjenja populacije vrste na osnovu uticaja toksina
-c = 0.5 # stopa izumiranja toksina
+a = 0.8 # prirodni prirastaj
+b = 0.2 # stopa umanjenja populacije vrste na osnovu uticaja toksina
+c = 0.7 # stopa izumiranja toksina
 d = 0.5 # stopa nastajanja toksina zavisi samo od kolicine populacije vrste
 
 #dX/dt = a*X - b*X*Y
 #dY/dt = -c*Y + d*X
 def dX_dt(X, t=0):
-    return array([ a*X[0] - b*X[0]*X[1] ,  
+    return array([ a*X[0] - b*X[0]*X[1] ,
                   -c*X[1] + d*X[0]      ])
 
 # stacionarne tacke
@@ -23,7 +23,7 @@ from scipy import integrate
 # vreme
 t = linspace(0, 100,  1000)
 # pocetne tacke
-X0 = array([2, 1])
+X0 = array([2, 0.5])
 
 X = integrate.odeint(dX_dt, X0, t)
 
@@ -36,12 +36,12 @@ p.grid()
 p.legend(loc='upper right')
 p.xlabel('vreme')
 p.ylabel('populacija')
-p.title(f'a: {a}, b: {b}, c: {c}, d: {d}')
-#f1.savefig('./plots/model1.png')
-p.show()
+p.title(f'MODEL: (a: {a}, b: {b}, c: {c}, d: {d}) \n ST: ({round((c*a) / (d*b), 2)}, {round(a/b, 2)})')
+#f1.savefig('./plots/model2.png')
+#p.show()
+
 
 #############################################################################################
-'''
 #vrednosti izmedju dve stacionarne tacke (0, c/d)
 values  = linspace(0, 3, 8)
 vcolors = p.cm.autumn_r(linspace(0., 1., len(values)))
@@ -70,7 +70,7 @@ DX1 /= M
 DY1 /= M                                  
 
 #iscrtavanje grafika
-p.title(f'Trajectories: a: {a}, b: {b}, c: {c}, d: {d}')
+p.title(f'MODEL: (a: {a}, b: {b}, c: {c}, d: {d}) \n ST: ({round((c*a) / (d*b), 2)}, {round(a/b, 2)})')
 Q = p.quiver(X1, Y1, DX1, DY1, M, pivot='mid', cmap=p.cm.jet)
 p.xlabel('Alge')
 p.ylabel('Toksini')
@@ -80,4 +80,3 @@ p.xlim(0, xmax)
 p.ylim(0, ymax)
 p.show()
 #f2.savefig('plots/populations.png')
-'''
